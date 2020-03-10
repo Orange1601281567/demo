834b530da95fc397781a6ae7e1bd2c622c508283
谌志清 <https://github.com/Orange1601281567/demo>
2020-03-10 14:22:27 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
                 " http://www.edu.gov.cn \n" +
 
                 "--------------------------------------  ");
-        System.out.println("我很开心 ");
+        System.out.println("我很开心! ");
     }
 
 }
