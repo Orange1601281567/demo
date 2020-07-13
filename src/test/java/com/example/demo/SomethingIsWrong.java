3d53556117d1cea3e8537d8754e624de74bb9caf
谌志清 <https://github.com/Orange1601281567/demo>
2020-07-13 16:30:43 +0800

    测试自动部署06

--- src/test/java/com/example/demo/SomethingIsWrong.java
+++ src/test/java/com/example/demo/SomethingIsWrong.java
@@ -12,7 +12,7 @@ public class SomethingIsWrong {
             for (int j = 0; j < 5; j++) {
                 System.out.print(arr[i][j]+"\t");
             }
-            System.out.println("测试自动部署!");
+            System.out.println("测试自动部署");
         }
 //        for (int i=1;i<10;i++){
 //            for (int j = 1; j <=i; j++) {
